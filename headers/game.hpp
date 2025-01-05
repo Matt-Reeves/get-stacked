fe10ea2eb19dec1d031809a1765daa8c378efb71
Matt Reeves <matt.reeves@data61.csiro.au>
2025-01-05 17:29:55 +1000

    Tidied up folder and build structure

---
+++ headers/game.hpp
@@ -0,0 +1,17 @@
+#include <SDL2/SDL.h>
+
+#include "level.hpp"
+#include "player.hpp"
+
+class Game {
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        Player player;
+        
+
+    public:
+
+    
+
+};
