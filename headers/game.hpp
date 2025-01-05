b3a43c980d632e6798a2988f9b2d2c74910c0609
Matt Reeves <matt.reeves@data61.csiro.au>
2025-01-05 17:39:20 +1000

    Added header for Game class

--- headers/game.hpp
+++ headers/game.hpp
@@ -2,15 +2,29 @@
 
 #include "level.hpp"
 #include "player.hpp"
+#include "LTimer.hpp"
 
 class Game {
     private:
+        bool quit = false;
+        int countedFrames = 0;
+        
+        Player player;
+        Level level;
+        
         SDL_Window* window;
         SDL_Renderer* renderer;
-        Player player;
+        SDL_Event e;
         
+        LTimer fpsTimer, capTimer;
 
     public:
+        bool init();
+        //bool loop();
+        void update(float dt);
+        void render();
+        bool cleanup();
+        void buildLevel();
 
     
 
